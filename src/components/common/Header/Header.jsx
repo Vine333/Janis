fcb2ea3277dbcf19663463cc9b0da0129cf83bda
Woody <woodywinterrrr@gmail.com>
2025-03-19 20:41:21 +0200

    v1.1

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, {useEffect} from "react";
 import styled from "styled-components";
 import {PhoneOutlined} from "@ant-design/icons";
 import {gsap} from "gsap";
-import Logo from '/public/assets/icons.svg'
+import Logo from '/public/icons.svg'
 
 const Header = () => {
 
